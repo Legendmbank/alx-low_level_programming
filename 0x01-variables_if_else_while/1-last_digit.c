f97f43ca8d9c1441fec07472c024dee030845a22
Blessing Darren IKPONMWOSA <legendmbank@gmail.com>
2022-07-12 18:39:15 -0700

    lowercase alpabet

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,7 +6,7 @@
  * main - Prints the last digit of a randomly generated number
  * and whether it is greater than 5, less than 6, or 0.
  * Return: Always 0.
- * */
+ */
 
 
 
@@ -14,7 +14,9 @@ int main(void)
 
 {
 	int n;
+
 	srand(time(0));
+
 	n = rand() - RAND_MAX / 2;
 
 
