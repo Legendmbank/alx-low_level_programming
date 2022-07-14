3afb67e8696cc88ae16ac9ec7ef65802355ba56a
Blessing Darren IKPONMWOSA <legendmbank@gmail.com>
2022-07-14 10:19:49 -0700

    check digit from 0 through 9

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _isdigit - check the code
+ * @c: int to be checked
+ * Return: successful digit is 1 and 0 if otherwise
+ */
+
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
