e1ed8a9b119eb598841580491335c5fa1438dade
Blessing Darren IKPONMWOSA <legendmbank@gmail.com>
2022-07-13 22:04:23 -0700

    debugging task 02 main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,3 +1,13 @@
+#ifndef MAIN_H 
+ 
+#define MAIN_H 
+ 
+#include <stdio.h> 
+ 
+void positive_or_negative(int i); 
+int largest_number(int a, int b, int c); 
+int convert_day(int month, int day); 
+void print_remaining_days(int month, int day, int year);
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -11,3 +21,5 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
+
+#endif /* MAIN_H */
